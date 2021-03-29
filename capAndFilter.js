fb011cbd2a1c863c6f37e2f181fb705c39dfd44d
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-03-29 12:11:00 -0700

    attempting for loop

---
+++ capAndFilter.js
@@ -0,0 +1,12 @@
+const capitalizeAndFilter = (array) => {
+  const newArray = [];
+
+  for(let i = 0; i < array.length; i++) {
+    const updated = array[i].charAt(0).toUpperCase() + array[i].slice(1);
+
+    if(updated.charAt(0) !== 'F')
+      newArray.push(updated);
+  }
+};
+
+module.exports = capitalizeAndFilter;
